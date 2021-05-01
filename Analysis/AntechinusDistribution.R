d3d3d6495e565a4bd87fb1927607582a936374de
Pietro <pietroviama@hotmail.com>
2021-05-01 16:51:44 +1000

    Zool J review

--- Analysis/AntechinusDistribution.R
+++ Analysis/AntechinusDistribution.R
@@ -1,7 +1,7 @@
 library(ggplot2)
 library(ggmap)
 library(maps)
-
+library(gghighlight)
 
 
 range(antechinusdata$Longitude, na.rm = TRUE, finite = TRUE)
@@ -14,11 +14,21 @@ map1
 
 map2 <- 
   map1+
-  geom_point(data=antechinusdata, 
-             aes(x=Longitude, y=Latitude, fill = Population, shape = Population), color="white", cex=6) +
-  scale_fill_manual(values = c("grey", "red", "blue", "hotpink", "darkgreen", "orange"), labels=c("?", "stuartii north", "stuartii south", "stuartii neotype", "subtropicus", "subtropicus holotype"), name=NULL) +
-  scale_shape_manual(values = c(25,24,23,21,22,21), labels=c("?", "stuartii north", "stuartii south", "stuartii neotype", "subtropicus", "subtropicus holotype"), name=NULL) + # define shape/color scales
-  labs(x="Longitude", y="Latitude", title="Antechinus distribution map") + # label the axes
-  theme_bw() + theme(legend.position="bottom", axis.text = element_text(size = 15), text = element_text(size = 15), legend.key = element_rect(colour = "white"), axis.text.x = element_text(angle=45, vjust=0.5), legend.text = element_text(size = 15)) # tweak the plot's appearance and legend position
+  geom_point(data = antechinusdata, 
+             aes(x=Longitude, y=Latitude, fill = Population, shape = Sex),  cex=6, alpha = 0.9, stroke = 2) +
+    scale_shape_manual(values = c(7, 12, 21, 21, 21), breaks = c("A. stuartii neotype", "A. subtropicus holotype"),
+                     labels = c((expression(paste(italic("A. stuartii"), " neotype"))),
+                                (expression(paste(italic("A. subtropicus"),  " holotype")))),
+                     guide = guide_legend(override.aes = list(alpha = 1, size = 6))) +
+  scale_fill_manual(values = c("darkgoldenrod2", "deeppink", "darkgreen", "#ced0ce"), breaks = c("A. stuartii south", "A. stuartii north", "A. subtropicus", "Unknown"),
+                    labels = c((expression(paste(italic("A. stuartii"), " south"))),
+                               (expression(paste(italic("A. stuartii"), " north"))),
+                               (expression(paste(italic("A. subtropicus")))),
+                               (expression(paste("Unknown"))))) +
+  labs(x="Longitude", y="Latitude", fill = "", shape = "") + # label the axes
+  theme_bw() + theme(legend.position="bottom", legend.box = "vertical", axis.text = element_text(size = 15), text = element_text(size = 15), legend.key = element_rect(colour = "white"), axis.text.x = element_text(angle=45, vjust=0.5), legend.text = element_text(size = 15)) +
+  guides(fill=guide_legend(override.aes=list(shape=21)))# tweak the plot's appearance and legend position
 
 map2
+
+
